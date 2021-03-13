261eda8984459362a2af9c3f1b05626ab456da20
svijsv <local>
2021-03-13 17:24:11 -0500

    Clean up PlatformIO ini files

--- tools/version.py
+++ tools/version.py
@@ -7,4 +7,6 @@ try:
 except:
 	version = "0000000"
 
-print('-DPROGNAME="\\\"{}\\\"" -DPROGVERS="\\\"{}\\\"" -DBUILD_DATE="\\\"{}\\\""'.format("GHMon", version, datetime.today().ctime()))
+# Using datetime.today().ctime() will cause everything to be rebuilt every
+# time because compiler flags change
+print('-DPROGVERS="\\\"{}\\\"" -DBUILD_DATE="\\\"{}\\\""'.format(version, datetime.today().strftime("%a %b %d %Y")))
